b7fb929bdec91e88ac9a7d8cdf8857102c726cfe
TarifSadman <sadmantaha17@gmail.com>
2024-05-09 01:40:18 +0600

    favorite movie list update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// React router
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
